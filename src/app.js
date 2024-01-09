25c9bcd680e30bc33273bb39a0be4303759e8c3b
Geraldine <c4dn5wld3@vossie.net>
2024-01-09 16:12:13 +0200

    API Integration

--- src/app.js
+++ src/app.js
@@ -1,13 +1,30 @@
 // when targetting classes we will use ".city"
 // when targetting id we will use "#"
 
+//API Integration
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "t6b6bfd88092a3a53d3o114058062b74";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
 // Search Engine
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Sandton");
