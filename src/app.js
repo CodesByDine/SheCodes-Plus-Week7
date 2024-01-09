8b0bf3cb2114ac47dda09c351510989443b28567
Geraldine <c4dn5wld3@vossie.net>
2024-01-09 15:47:36 +0200

    Create Search Engine

---
+++ src/app.js
@@ -0,0 +1,13 @@
+// when targetting classes we will use ".city"
+// when tragetting id we will use "#"
+
+// Search Engine
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
